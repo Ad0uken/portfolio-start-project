942d77ae5f60df3003a06417193adef9f4795119
Ad0uken <a9997009915@gmail.com>
2024-02-23 13:13:07 +0300

    33

---
+++ src/layout/sections/works/Works.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Menu } from '../../../components/menu/Menu';
+
+
+export const Works = () => {
+    return (
+        <StyledWorks>
+                <SectionTitle>My Works</SectionTitle>
+
+                
+        </StyledWorks>
+    );
+};
+
+const StyledWorks = styled.section`
+    min-height: 100vh;
+    background-color: #deefff;
+`
