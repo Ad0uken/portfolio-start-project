3dde6e94a06536f911d92e04bd5602327f58518e
Ad0uken <a9997009915@gmail.com>
2024-02-23 13:31:58 +0300

    3333

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Menu } from '../../../components/menu/Menu';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import socialImg from '../../../assets/images/proj1.webp'
+import timerImg from '../../../assets/images/proj2.webp'
+import { Work } from './work/Work';
 
 const workItems = ['All','Landing Page','React','Spa']
 
@@ -11,6 +15,10 @@ export const Works = () => {
                 <SectionTitle>My Works</SectionTitle>
 
                 <Menu menuItems={workItems}/>
+                <FlexWrapper>
+                    <Work title={'Social Network'} text={'some text'} src={socialImg}/>
+                    <Work title={'Timer'} text={'some text 2'} src={timerImg}/>
+                </FlexWrapper>
         </StyledWorks>
     );
 };
