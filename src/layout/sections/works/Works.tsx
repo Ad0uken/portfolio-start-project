fb85cb09dae0d8c331dcae7d7aebdf5a4c402e91
Ad0uken <a9997009915@gmail.com>
2024-02-23 13:15:30 +0300

    333

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Menu } from '../../../components/menu/Menu';
 
+const workItems = ['All','Landing Page','React','Spa']
 
 export const Works = () => {
     return (
         <StyledWorks>
                 <SectionTitle>My Works</SectionTitle>
 
-                
+                <Menu menuItems={workItems}/>
         </StyledWorks>
     );
 };
